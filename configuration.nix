cc1f4cf6ad9df828ae248afd618a7fd7c167a377
Nixos <monand@tuta.io>
2024-06-22 13:31:16 +0200

    config cleanup

--- configuration.nix
+++ configuration.nix
@@ -1,46 +1,15 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 { config, lib, pkgs, ... }:
 
 {
-  imports = [
-      (./hardware-configuration.nix)
-    ];
+  imports = [ (./hardware-configuration.nix) ];
 
-  # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  networking.hostName = "nixos"; # Define your hostname.
-  networking.networkmanager.enable = true;
-  networking.networkmanager.ensureProfiles.profiles = {
-    "fiberway.pl_20a_2_5GHz" = {
-      connection = {
-        id = "fiberway.pl_20a/2_5GHz";
-        interface = "wlp2s0";
-        type = "wifi";
-      };wifi = {
-        mode = "infrastructure";
-        ssid = "fiberway.pl_20a/2_5GHz";
-        auth-alg = "open";
-        key-mgmt = "wpa-psk";
-        psk = (builtins.readFile ./wifi_passwd);
-      };
-    };
-  };
-
-  time.timeZone = "Europe/Warsaw";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-  console = {
-    font = "Lat2-Terminus16";
-    useXkbConfig = true; # use xkb.options in tty.
-  };
+  # Copy the NixOS configuration file and link it from the resulting system (/run/current-system/configuration.nix). 
+  system.copySystemConfiguration = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users = {
@@ -48,13 +17,14 @@
       isNormalUser = true;
       home = "/home/nixos";
       description = "Nixos";
-      extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+      extraGroups = [ "wheel" "docker" ];
     };
     git = {
       isNormalUser = true;
       home = "/home/git";
       description = "Git";
-      openssh.authorizedKeys.keyFiles = [
+      extraGroups = [ "docker" ]; 
+    openssh.authorizedKeys.keyFiles = [
         /home/git/.ssh/authorized_keys
       ];
     };
@@ -63,9 +33,15 @@
     ];
   };
 
-  # Allow unfree software
-  nixpkgs.config.allowUnfree = true;
+  # Personalization
+  time.timeZone = "Europe/Warsaw";
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    useXkbConfig = true; 
+  };
 
+  nixpkgs.config.allowUnfree = true;
   # List packages installed in system profile. To search, run: $ nix search wget
   environment.systemPackages = with pkgs; [
     vim 
@@ -89,10 +65,16 @@
     niv
     git-credential-manager
     dotnet-runtime_7
+    lsof
+    cron
   ];
 
   environment.sessionVariables = {
   DOTNET_ROOT = "${pkgs.dotnet-sdk_7}";
+  };
+
+  services.cron = {
+    enable = true;
   };
 
   programs.git = {
@@ -109,7 +91,29 @@
 
   programs.java.enable = true;
 
-  # Enable the OpenSSH daemon.
+  # Networks
+  networking.hostName = "nixos";
+  networking.networkmanager.enable = true;
+  networking.networkmanager.ensureProfiles.profiles = {
+    "fiberway.pl_20a_2_5GHz" = {
+      connection = {
+        id = "fiberway.pl_20a/2_5GHz";
+        interface = "wlp2s0";
+        type = "wifi";
+      };wifi = {
+        mode = "infrastructure";
+        ssid = "fiberway.pl_20a/2_5GHz";
+        auth-alg = "open";
+        key-mgmt = "wpa-psk";
+        psk = (builtins.readFile ./wifi_passwd);
+      };
+    };
+  };
+
+  networking.firewall.enable = true;
+  networking.firewall.allowedTCPPorts = [ 80 443 22 8080 ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+
   services.openssh = {
     enable = true;
     settings.PermitRootLogin = "yes";
@@ -117,21 +121,6 @@
     settings.KbdInteractiveAuthentication = false;
   };
 
-  # Open ports in the firewall.
-  networking.firewall.enable = true;
-  networking.firewall.allowedTCPPorts = [ 80 443 22 8080 ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-
-  # Copy the NixOS configuration file and link it from the resulting system (/run/current-system/configuration.nix). 
-  # This is useful in case you accidentally delete configuration.nix.
-  system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "23.11"; # Did you read the comment?
-
+  system.stateVersion = "23.11"; # NEVER CHANGE
 }
 
